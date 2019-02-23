a74c9a187a795c85862286e7b6d2b095c503041e
Alberto Morales <cmtmorales24@gmail.com>
2019-02-23 22:02:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,9 +4,10 @@ def loop_iterator(number_of_times)
     x=0
     loop do
       puts"Welcome to Flatiron School's Web Development Course!"
-      x+=1
-    end
-      if x==number_of_times break
+      x=x+1
+    if x==number_of_times 
+        break
+      end
  end
  end 
 def times_iterator(number_of_times)
